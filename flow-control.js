6815663eb0dc8e3627c84bdc6e6b146a7676185e
Kavachi Ukegbu <michelle.ukegbu@gmail.com>
2020-03-26 17:22:06 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -19,16 +19,9 @@ function ageChecker(age) {
   if ( age >= 13 && age <= 19) {
     return "You are a teenager!"
 
-  } else if (age >= 12) {
-    return "You are a kid"
-
-  } else( age >= 19) {
-    return "You are a grownup"
-
   }
-  {
 
-}
+
 
 function ternaryTeenager(age) {
 
